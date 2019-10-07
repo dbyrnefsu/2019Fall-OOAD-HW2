e97cc1f1f3362c08229dff45580f5dc1377253ef
dmallepa <55263982+dmallepa@users.noreply.github.com>
2019-10-06 21:00:15 -0400

    Update Wood.java

--- src/edu/fitchburgstate/csc7400/hw2/Wood.java
+++ src/edu/fitchburgstate/csc7400/hw2/Wood.java
@@ -1,5 +1,17 @@
+/**
+ * Class: Object-Oriented Design and Analysis
+ * Professor: Orlando Montalvo
+ * Assignment: HW 2
+ * 
+ * Students: Abha Kumari, Divya Mallepally, Vineela Venula 
+ * Date: 2019-10-06
+ */
+ 
 package edu.fitchburgstate.csc7400.hw2;
 
+/**
+ *  Wood that can be used
+ */
 public enum Wood {
 	INDIAN_ROSEWOOD,
 	BRAZILIAN_ROSEWOOD,

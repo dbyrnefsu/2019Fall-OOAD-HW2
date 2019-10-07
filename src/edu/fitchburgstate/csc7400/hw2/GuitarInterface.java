858dafe571a2e6cd6233676029ce0333b6f9fb5f
dmallepa <55263982+dmallepa@users.noreply.github.com>
2019-10-06 20:57:24 -0400

    Update GuitarInterface.java

--- src/edu/fitchburgstate/csc7400/hw2/GuitarInterface.java
+++ src/edu/fitchburgstate/csc7400/hw2/GuitarInterface.java
@@ -1,3 +1,12 @@
+/**
+ * Class: Object-Oriented Design and Analysis
+ * Professor: Orlando Montalvo
+ * Assignment: HW 2
+ * 
+ * Students: Abha Kumari, Divya Mallepally, Vineela Venula 
+ * Date: 2019-10-06
+ */
+
 package edu.fitchburgstate.csc7400.hw2;
 
 public interface GuitarInterface {

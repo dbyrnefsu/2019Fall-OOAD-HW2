7893a023b431860e8a04ae7864a289b19b65f1ec
dmallepa <55263982+dmallepa@users.noreply.github.com>
2019-10-06 20:56:18 -0400

    Update GuitarInterface.java

--- src/edu/fitchburgstate/csc7400/hw2/GuitarInterface.java
+++ src/edu/fitchburgstate/csc7400/hw2/GuitarInterface.java
@@ -1,14 +1,29 @@
 package edu.fitchburgstate.csc7400.hw2;
 
-public interface GuitarInterface {	
-
-	public Manufacturer getGuitarManufacturer();	
-
-	public Type getGuitarType();	
-
+public interface GuitarInterface {
+	
+	/**
+	 * Returns the manufacturer model
+	 */
+	public Manufacturer getGuitarManufacturer();
+	
+	/**
+	 * Returns the guitar type	
+	 */	
+	public Type getGuitarType();
+	
+	/**
+	 * Returns the type of wood used in the face
+	 */
 	public Wood getGuitarTopWood();
-
-	public Wood getGuitarBackWood();	
-
+	
+	/**
+	 * Returns the type of wood used in the body
+	 */
+	public Wood getGuitarBackWood();
+	
+	/**
+	 * Returns the manufacturer model
+	 */
 	public String getGuitarModel();
 }

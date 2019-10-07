4f5e2ba4f611a0da935a73cfbdd423219d876a09
dmallepa <55263982+dmallepa@users.noreply.github.com>
2019-10-06 21:00:01 -0400

    Update Type.java

--- src/edu/fitchburgstate/csc7400/hw2/Type.java
+++ src/edu/fitchburgstate/csc7400/hw2/Type.java
@@ -1,5 +1,17 @@
+/**
+ * Class: Object-Oriented Design and Analysis
+ * Professor: Orlando Montalvo
+ * Assignment: HW 2
+ * 
+ * Students: Abha Kumari, Divya Mallepally, Vineela Venula 
+ * Date: 2019-10-06
+ */
+
 package edu.fitchburgstate.csc7400.hw2;
 
+/**
+ *  Type that can be used
+ */
 public enum Type {
 	ELECTRIC,
 	ACOUSTIC

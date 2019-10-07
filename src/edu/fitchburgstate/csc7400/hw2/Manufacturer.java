8a7f515cb0f8868b67d8c0793097f99efecb0d6b
dmallepa <55263982+dmallepa@users.noreply.github.com>
2019-10-06 20:59:45 -0400

    Update Manufacturer.java

--- src/edu/fitchburgstate/csc7400/hw2/Manufacturer.java
+++ src/edu/fitchburgstate/csc7400/hw2/Manufacturer.java
@@ -1,5 +1,18 @@
+/**
+ * Class: Object-Oriented Design and Analysis
+ * Professor: Orlando Montalvo
+ * Assignment: HW 2
+ * 
+ * Students: Abha Kumari, Divya Mallepally, Vineela Venula 
+ * Date: 2019-10-06
+ */
+
+
 package edu.fitchburgstate.csc7400.hw2;
 
+/**
+ *  Manufacturer that can be used
+ */
 public enum Manufacturer {
 	FENDER,
 	MARTIN,

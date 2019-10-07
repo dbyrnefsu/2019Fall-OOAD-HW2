7dac499a9873a742b469389560d9202378610897
sravaniveerapu <55327860+sravaniveerapu@users.noreply.github.com>
2019-10-06 23:12:41 -0400

    Updated GuitarInterface
    Added Comments

--- src/edu/fitchburgstate/csc7400/hw2/GuitarInterface.java
+++ src/edu/fitchburgstate/csc7400/hw2/GuitarInterface.java
@@ -1,14 +1,22 @@
 package edu.fitchburgstate.csc7400.hw2;
-
+/**
+ * Class        : Object-Oriented Analysis and Design
+ * Professor    : Orlando Montalvo
+ * Assignment   : HW-2
+ * Date         : 10-06-2019
+ * @author      : Sravani Veerapureddy and Yamini Gopisetti
+ */
+ 
+ 
 public interface GuitarInterface {
-
+         //Returns the name of the manufacturer
 	Manufacturer getManufacturer();
-
+         //Returns the guitar type
 	Type getType();
-
+         //Returns the type of wood used in the body
 	Wood getTopWood();
-
+         //Returns the type of wood used in the body
 	Wood getBackWood();
-
+         //Returns the manufacturer model
 	String getModel();
 }
